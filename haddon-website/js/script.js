ec394d1a109b80ba5bbac4076141bc4a3811f923
Tony Haddon <tony.haddon@gmail.com>
2012-08-29 21:39:37 +0100

    Some SEO tweaks

--- haddon-website/js/script.js
+++ haddon-website/js/script.js
@@ -1,8 +1,4 @@
-/* Author:
-
-*/
-
-
-
-
-
+function recordOutboundLink(link, category, action, opt_label, opt_value) {
+_gat._getTrackerByName()._trackEvent(category, action, opt_label, opt_value);
+setTimeout('document.location = "' + link.href + '"', 100);
+}
